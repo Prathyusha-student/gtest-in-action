365459bf7c06c91ecece8e0f0bbb85836a823f7f
Prathyusha Reddy <prathyushaprathyusha757@gmail.com>
2024-09-17 14:35:42 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -15,7 +15,7 @@ TEST(StringCalculatorTestSuite,add_emptyInputString_ZeroIsExpected)
 TEST(StringCalculatorTestSuite,add_ZeroInputString_ZeroIsExpected)
 {
   //Arrange
-  string input= 0;
+  string input= "0";
   int expectedValue = 0;
   //ACT
   int actualValue = Add(input);
