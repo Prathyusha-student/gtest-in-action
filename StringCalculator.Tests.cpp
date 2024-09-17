24b0cf5dc91bca47064886bd3bf8e12a22b001ee
Prathyusha Reddy <prathyushaprathyusha757@gmail.com>
2024-09-17 15:10:31 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -38,7 +38,7 @@ TEST(StringCalculatorTestSuite,add_NegativeString_NegativeIsExpected)
 {
   //Arrange
   string input= "-1";
- string expectedValue = "Negatives not allowed";
+const char* expectedValue = "Negatives not allowed";
   //ACT
   int actualValue = Add(input);
   //Assert
