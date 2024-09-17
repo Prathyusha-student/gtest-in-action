3bc78216039fd34e3c9fc5b894ecc9868d4fdc75
Prathyusha Reddy <prathyushaprathyusha757@gmail.com>
2024-09-17 17:17:41 +0530

    Update StringCalculatorDataDriven.Tests.cpp

--- StringCalculatorDataDriven.Tests.cpp
+++ StringCalculatorDataDriven.Tests.cpp
@@ -7,23 +7,32 @@ public:
       TestDataPair(string _input,int _expectedValue) : input(_input),expectedValue(_expectedValue){
       }    
 };
-TEST(StringCalculatorDataDrivenTestSuite, DataDrivenTestCase){
+/*TEST(StringCalculatorDataDrivenTestSuite, DataDrivenTestCase){
 vector<TestDataPair> dataList;
 TestDataPair pair_one{ "",0};
 TestDataPair pair_two{ "0",0};
 TestDataPair pair_three{ "1",1};
-TestDataPair pair_four{ "1,2,3",6};
+TestDataPair pair_four{ "1,2,3",6};*/
 
+vector<TestDataPair> dataList;
 dataList.push_back(pair_one);
 dataList.push_back(pair_two);
 dataList.push_back(pair_three);
 dataList.push_back(pair_four);
+TEST_F(StringCalculatorTest, DataDrivenTestCase) {
+    // Iterate using a range-based for-loop
+    for (TestDataPair dataPair : dataList) {
+        int actualValue = Add(dataPair.input);
+        ASSERT_EQ(actualValue, dataPair.expectedValue);
+    }
+}
 
-//Iteratw using a range-based for-loop
+/*Iteratw using a range-based for-loop
 for(TestDataPair dataPair : dataList){
 int actualValue= Add(dataPair.input);
 ASSERT_EQ(actualValue,dataPair.expectedValue);
 }
-}
+}*/
+
 
 
