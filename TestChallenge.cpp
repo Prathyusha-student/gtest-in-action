e4b3c4b0a6c1a38af7e76db7cd5ccff2f25bd36e
Prathyusha Reddy <prathyushaprathyusha757@gmail.com>
2024-09-18 17:13:45 +0530

    Update TestChallenge.cpp

--- TestChallenge.cpp
+++ TestChallenge.cpp
@@ -7,7 +7,7 @@ int alertFailureCount = 0;
 int getFailureCount(){
 return alertFailureCount;
 }
- 
+
 void alertInCelcius(float farenheit,INetworkAlerter *networkAlerterPtr) {
     float celcius = (farenheit - 32) * 5 / 9;
     int returnCode = networkAlerterPtr->alert(celcius);
