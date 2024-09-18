08f1bc479db5425560b929db61c29943046aa9b4
Prathyusha Reddy <prathyushaprathyusha757@gmail.com>
2024-09-18 16:56:46 +0530

    Update TestChallenge.h

--- TestChallenge.h
+++ TestChallenge.h
@@ -1,2 +1,11 @@
 //void alertInCelcius(float farenheit);
-int alertInCelciusFacade(float farenheit);
+//int alertInCelciusFacade(float farenheit);
+
+//Abstraction using Class Interface
+// functionpointer
+class INetworkAlerter{
+public:
+    virtual void alert(float celcius)=0;
+};
+
+int alertInCelciusFacade(float farenheit,INetworkAlerter *networkAlerterPtr);
